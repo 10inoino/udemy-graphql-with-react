bbb205f678e59e304ee334280bda98fccbc04220
Hikaru Inomata <inomatahi@icloud.com>
2022-06-05 06:15:56 +0000

    リポジトリ取得のクエリと変数の追加

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
 import { useQuery } from "@apollo/client";
-import { ME } from "./graphql";
+import { Component } from "react";
+import { SEARCH_REPOSITORIES } from "./graphql";
 
-function GetAccountData() {
-  const { loading, error, data } = useQuery(ME);
+const VARIABLES = {
+  first: 5,
+  after: null,
+  last: null,
+  before: null,
+  query: "react",
+};
+
+function GetRepositories(props) {
+  const { loading, error, data } = useQuery(SEARCH_REPOSITORIES, {
+    variables: props,
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
-  const user = data.user;
 
-  return (
-    <div>
-      {user.name}: {user.avatarUrl}
+  const edges = data.search.edges;
+
+  return edges.map(({ cursor, node }) => (
+    <div key={cursor}>
+      <h1>{node.id}</h1>
+      <p>{node.name}</p>
+      <p>{node.url}</p>
+      <p>{node.viewerHasStarred ? "true" : "false"}</p>
     </div>
-  );
+  ));
 }
 
-function App() {
-  return (
-    <div>
-      Hello, GraphQL
-      <GetAccountData />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = VARIABLES;
+  }
+
+  render() {
+    return (
+      <div>
+        <GetRepositories
+          first={VARIABLES.first}
+          after={VARIABLES.after}
+          last={VARIABLES.last}
+          before={VARIABLES.before}
+          query={VARIABLES.query}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/client.js
+++ src/client.js
@@ -1,13 +1,9 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  createHttpLink
-} from "@apollo/client";
-import { setContext } from '@apollo/client/link/context';
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
-const endpoint = "https://api.github.com/graphql"
+const endpoint = "https://api.github.com/graphql";
 
 const httpLink = createHttpLink({
   uri: endpoint,
@@ -18,14 +14,14 @@ const authLink = setContext((_, { headers }) => {
     headers: {
       ...headers,
       authorization: `Bearer ${GITHUB_TOKEN}`,
-    }
-  }
+    },
+  };
 });
 
 const link = authLink.concat(httpLink);
 
 export default new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
   link: link,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
